7f58a25fcd85359a5c6851b9adea33cd4e82d40e
unknown <MuhammadAhmad338@github.com>
2023-12-28 20:08:17 +0500

    latest latest commits

---
+++ Controller/profileController.js
@@ -0,0 +1,33 @@
+const app = require("../api/api");
+const {getStorage, uploadBytes, getDownloadURL, ref, uploadString} = require("firebase/storage");
+
+const storage = getStorage(app);
+
+const uploadImage = async (req, res) => {
+    const { image } = req.body;
+   console.log(image);
+    try {
+        
+        const storageRef = ref(storage, 'profile_pics/');
+        await uploadString(storageRef, image);
+        res.status(200).json({message: "Image uploaded successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ message: `Some error Occured ${error}` });
+    }
+};
+
+//Download Image
+const downloadImage = async (req, res) => {
+    const { image } = req.body;
+    try {
+        const storageRef = ref(storage, 'profile_pics/' + image);
+        const url = await getDownloadURL(storageRef);
+        res.status(200).json({message: "Image uploaded successfully", url});
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ message: `Some error Occured ${error}` });
+    }
+};
+
+module.exports = { uploadImage, downloadImage };
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = require("./Routes/userRoutes");
 const palmRouter = require("./Routes/palmRoutes");
 const tipsRouter = require("./Routes/tipsRoutes");
+const imageRouter = require("./Routes/imageRoutes");
 const app = express();
 
 const PORT = parseInt(process.env.PORT) || 8080;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use("/api/users", router);
 app.use("/api", palmRouter);
 app.use("/api", tipsRouter);
+app.use("/api", imageRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is Listening at the the port ${PORT}`);
